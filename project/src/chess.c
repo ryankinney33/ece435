c87d90eea5097d55a09195a5c3d15fe8b2da36ea
Ryan Kinney <ryan.kinney@maine.edu>
2022-03-30 13:41:52 -0400

    Improve the grid with alternating colors

--- project/src/chess.c
+++ project/src/chess.c
@@ -1,27 +1,60 @@
 #include <ncurses.h>
 
+#define WHITE_BG 1
+#define BLACK_BG 2
+
 static int display_grid() {
 	clear();
-	printw("   A  B  C  D  E  F  G\n\n");
-	printw("8                        8\n");
-	printw("7                        7\n");
-	printw("6                        6\n");
-	printw("5                        5\n");
-	printw("4                        4\n");
-	printw("3                        3\n");
-	printw("2                        2\n");
-	printw("1                        1\n\n");
-	printw("   A  B  C  D  E  F  G");
+/*	printw("8         8\n");
+	printw("7         7\n");
+	printw("6         6\n");
+	printw("5         5\n");
+	printw("4         4\n");
+	printw("3         3\n");
+	printw("2         2\n");
+	printw("1         1\n");
+	printw("  ABCDEFGH");
+*/
+	for (int i = 8; i > 0; --i) {
+		printw("%d ", i);
+		for (int j = 0; j < 8; ++j) {
+			int color = ((j+i) & 1) + 1;
+			printf("color: %d\r\n", color);
+			attron(COLOR_PAIR(color));
+			printw(" ");
+			attroff(COLOR_PAIR(color));
+		}
+		printw(" %d\n", i);
+	}
 
+	printw("  ABCDEFGH");
 	refresh();
+	return 0;
+}
+
+static int init_colors() {
+	if (!has_colors()) {
+		endwin();
+		return -1;
+	}
+
+	start_color();
+	init_pair(WHITE_BG, COLOR_WHITE, COLOR_WHITE);
+	init_pair(BLACK_BG, COLOR_BLACK, COLOR_BLACK);
+
+	return 0;
 }
 
 int main () {
 	// Start curses mode
 	initscr();
-	start_color();
-	display_grid();
+	curs_set(0);
+	if (init_colors()) {
+		fprintf(stderr, "Error: your terminal does not support color.\n");
+		return 1;
+	}
 
+	display_grid();
 	getch(); // wait for user input
 	endwin(); // end curses mode
 
