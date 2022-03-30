72621831b844fbedbcd0a459db84ca1a5b545a18
Ryan Kinney <ryan.kinney@maine.edu>
2022-03-30 12:21:15 -0400

    Preliminary grid display function

--- project/src/chess.c
+++ project/src/chess.c
@@ -1,10 +1,27 @@
 #include <ncurses.h>
 
+static int display_grid() {
+	clear();
+	printw("   A  B  C  D  E  F  G\n\n");
+	printw("8                        8\n");
+	printw("7                        7\n");
+	printw("6                        6\n");
+	printw("5                        5\n");
+	printw("4                        4\n");
+	printw("3                        3\n");
+	printw("2                        2\n");
+	printw("1                        1\n\n");
+	printw("   A  B  C  D  E  F  G");
+
+	refresh();
+}
+
 int main () {
 	// Start curses mode
 	initscr();
-	printw("AAAAAAAAAAAA");
-	refresh(); // display on the real screen
+	start_color();
+	display_grid();
+
 	getch(); // wait for user input
 	endwin(); // end curses mode
 
