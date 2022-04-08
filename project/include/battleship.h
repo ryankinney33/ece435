ce028adc4e839397c467e4086dcfe2a9277e41df
Ryan Kinney <ryan.kinney@maine.edu>
2022-04-08 14:03:35 -0400

    Update the battleship header

--- project/include/battleship.h
+++ project/include/battleship.h
@@ -6,4 +6,11 @@
 // Functions related to the beginning and end of the game
 struct team *init_game(int use_color);
 
+
+// Get a move from the user and send it to the remote client
+void get_move_user(struct team *btlshp);
+
+// Get a move from the remote client
+void get_move_enemy(struct team *btlshp);
+
 #endif /* BATTLESHIP_H */
